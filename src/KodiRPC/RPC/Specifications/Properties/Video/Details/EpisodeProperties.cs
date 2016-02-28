9136da27f3db044658bd738f074d4366a32f32b0
Pieter-Uys Fourie <pieteruys.fourie@gmail.com>
2016-02-28 20:14:15 +0200

    Implemented properties and response.

---
+++ src/KodiRPC/RPC/Specifications/Properties/Video/Details/EpisodeProperties.cs
@@ -0,0 +1,21 @@
+﻿using KodiRPC.Responses.Types.Video.Details;
+
+namespace KodiRPC.RPC.Specifications.Properties.Video.Details
+{
+    public class EpisodeProperties : File
+    {
+        public const string Cast = "cast";
+        public const string ProductionCode = "productioncode";
+        public const string Rating = "rating";
+        public const string Votes = "votes";
+        public const string EpisodeNumber = "episodenumber";
+        public const string ShowTitle = "showtitle";
+        public const string EpisodeId = "episodeid";
+        public const string TvshowId = "tvshowid";
+        public const string Season = "season";
+        public const string FirstAired = "firstaired";
+        public const string UniqueId = "uniqueid";
+        public const string Writer = "writer";
+        public const string OriginalTitle = "originaltitle";
+    }
+}
---
+++ src/KodiRPC/Responses/VideoLibrary/GetEpisodeDetailsResponse.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+using KodiRPC.Responses.Types.Video.Details;
+
+namespace KodiRPC.Responses.VideoLibrary
+{
+    public class GetEpisodeDetailsResponse
+    {
+        [JsonProperty(PropertyName = "episodedetails")]
+        public Episode Result { get; set; }
+    }
+}
