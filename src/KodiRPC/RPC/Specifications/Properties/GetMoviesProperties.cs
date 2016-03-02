f5b345335a93faeb662c9ae2f55e577110607a0a
Pieter-Uys Fourie <pieteruys.fourie@gmail.com>
2016-03-02 23:23:37 +0200

    Started implementing GetEpisodes, GetMovies and GetTvShows. #4 #5 #10.

---
+++ src/KodiRPC/RPC/Specifications/Properties/GetMoviesProperties.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using System.Reflection;
+using KodiRPC.RPC.Specifications.Properties.Video.Details;
+
+namespace KodiRPC.RPC.Specifications.Properties
+{
+    public class GetMoviesProperties : MovieProperties
+    {
+        public static string[] All()
+        {
+            var declaringType = MethodBase.GetCurrentMethod().DeclaringType;
+
+            return declaringType?.GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy)
+                .Select(field => field.GetValue(null) as string).ToArray();
+        }
+    }
+}
--- src/KodiRPC/Services/IKodiService.cs
+++ src/KodiRPC/Services/IKodiService.cs
@@ -13,14 +13,23 @@ namespace KodiRPC.Services
         #region VideoLibrary
 
         #region Shows
+        GetTvShowsResponse GetTvShows(string[] properties = null, string[] limits = null,
+            string sort = null, string filter = null, string requestId = "GetTvShowsResponse");
+
         GetTvShowDetailsResponse GetTvShowDetails(int tvShowId, string[] properties = null,
             string requestId = "GetTvShowDetailsResponse");
+
+        GetEpisodesResponse GetEpisodes(int tvShowId = -1, int seasonNumber = -1, string[] properties = null,
+            string[] limits = null, string[] sort = null, string[] filter = null, string requestId = "GetEpisodesResponse");
 
         GetEpisodeDetailsResponse GetEpisodeDetails(int epidodeId, string[] properties = null,
             string requestId = "GetEpisodeDetailsResponse");
         #endregion
 
         #region Movies
+        GetMoviesResponse GetMovies(string[] properties = null, string[] limits = null, 
+            string[] sort = null, string[] filter = null, string requestId = "GetMovieDetailsResponse");
+
         GetMovieDetailsResponse GetMovieDetails(int movieId, string[] properties = null,
             string requestId = "GetMovieDetailsResponse");
         #endregion
