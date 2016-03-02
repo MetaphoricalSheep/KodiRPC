f78be3a9b3e0925b90601c51d5f5f7329d80db4d
Pieter-Uys Fourie <pieteruys.fourie@gmail.com>
2016-03-02 22:04:37 +0200

    Integration test for GetTvShowDetails

---
+++ src/KodiRPC.Tests/Integration/GetTvShowDetailsTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using KodiRPC.RPC.Specifications.Properties.Video.Details;
+using KodiRPC.Services;
+using NUnit.Framework;
+
+namespace KodiRPC.Tests.Integration
+{
+    [ExcludeFromCodeCoverage]
+    public class GetTvShowDetailsTest
+    {
+        [Test]
+        public void ValidateTvShowObjectReturnedByService()
+        {
+            var service = new KodiService();
+            var properties = new[] { TvShowProperties.Title, TvShowProperties.ImdbNumber, TvShowProperties.Year };
+            var details = service.GetTvShowDetails(1, properties);
+
+            Assert.IsNotNull(details);
+            Assert.IsNotNull(details.Result);
+            Assert.AreEqual(1, details.Result.TvShowId);
+        }
+    }
+}
