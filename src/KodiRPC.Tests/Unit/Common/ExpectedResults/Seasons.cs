f8b1965de8e41fa8a33e3385009550b0a48d490f
Pieter-Uys Fourie <pieteruys.fourie@gmail.com>
2016-03-04 02:02:01 -0800

    Fixed #8 Added Season stuff. Added Season integration and unit tests

---
+++ src/KodiRPC.Tests/Unit/Common/ExpectedResults/Seasons.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using KodiRPC.Responses.Types.Media;
+using KodiRPC.Responses.Types.Video.Details;
+using KodiRPC.Responses.VideoLibrary;
+
+namespace KodiRPC.Tests.Unit.Common.ExpectedResults
+{
+    public class Seasons
+    {
+        public static GetSeasonsResponse GetList()
+        {
+            return new GetSeasonsResponse
+            {
+                Result = new List<Season>
+                {
+                    new Season
+                    {
+                        Art = new Artwork
+                        {
+                            Poster =
+                                "image://%2fmedia%2fgotham%2fseries%2fThe%20Americans%20(2013)%2fseason01-poster.jpg/",
+                            //Banner = "image://%2fmedia%2fgotham%2fseries%2fThe%20Americans%20(2013)%2fbanner.jpg/",
+                            //Fanart = "image://%2fmedia%2fgotham%2fseries%2fThe%20Americans%20(2013)%2ffanart.jpg/",
+                            //Poster = "image://%2fmedia%2fgotham%2fseries%2fThe%20Americans%20(2013)%2fposter.jpg/"
+                        },
+                        Episode = 13,
+                        Fanart = "image://%2fmedia%2fgotham%2fseries%2fThe%20Americans%20(2013)%2ffanart.jpg/",
+                        Label = "Season 1",
+                        Playcount = 0,
+                        SeasonNumber = 1,
+                        SeasonId = 380,
+                        ShowTitle = "The Americans (2013)",
+                        Thumbnail =
+                            "image://%2fmedia%2fgotham%2fseries%2fThe%20Americans%20(2013)%2fseason01-poster.jpg/",
+                        TvShowId = 62,
+                        WatchedEpisodes = 0
+                    },
+                    new Season
+                    {
+                        Art = new Artwork
+                        {
+                            Poster =
+                                "image://%2fmedia%2fgotham%2fseries%2fThe%20Americans%20(2013)%2fseason02-poster.jpg/",
+                            //Banner = "image://%2fmedia%2fgotham%2fseries%2fThe%20Americans%20(2013)%2fbanner.jpg/",
+                            //Fanart = "image://%2fmedia%2fgotham%2fseries%2fThe%20Americans%20(2013)%2ffanart.jpg/",
+                            //Poster = "image://%2fmedia%2fgotham%2fseries%2fThe%20Americans%20(2013)%2fposter.jpg/"
+                        },
+                        Episode = 13,
+                        Fanart = "image://%2fmedia%2fgotham%2fseries%2fThe%20Americans%20(2013)%2ffanart.jpg/",
+                        Label = "Season 2",
+                        Playcount = 0,
+                        SeasonNumber = 2,
+                        SeasonId = 381,
+                        ShowTitle = "The Americans (2013)",
+                        Thumbnail =
+                            "image://%2fmedia%2fgotham%2fseries%2fThe%20Americans%20(2013)%2fseason02-poster.jpg/",
+                        TvShowId = 62,
+                        WatchedEpisodes = 0
+                    },
+                    new Season
+                    {
+                        Art = new Artwork
+                        {
+                            Poster =
+                                "image://%2fmedia%2fgotham%2fseries%2fThe%20Americans%20(2013)%2fseason03-poster.jpg/",
+                            //Banner = "image://%2fmedia%2fgotham%2fseries%2fThe%20Americans%20(2013)%2fbanner.jpg/",
+                            //Fanart = "image://%2fmedia%2fgotham%2fseries%2fThe%20Americans%20(2013)%2ffanart.jpg/",
+                            //Poster = "image://%2fmedia%2fgotham%2fseries%2fThe%20Americans%20(2013)%2fposter.jpg/"
+                        },
+                        Episode = 13,
+                        Fanart = "image://%2fmedia%2fgotham%2fseries%2fThe%20Americans%20(2013)%2ffanart.jpg/",
+                        Label = "Season 3",
+                        Playcount = 0,
+                        SeasonNumber = 3,
+                        SeasonId = 382,
+                        ShowTitle = "The Americans (2013)",
+                        Thumbnail =
+                            "image://%2fmedia%2fgotham%2fseries%2fThe%20Americans%20(2013)%2fseason03-poster.jpg/",
+                        TvShowId = 62,
+                        WatchedEpisodes = 0
+                    }
+                }
+            };
+        }
+    }
+}
---
+++ src/KodiRPC.Tests/Unit/GetSeasonsTests.cs
@@ -0,0 +1,31 @@
+﻿using KodiRPC.Responses.VideoLibrary;
+using KodiRPC.RPC.RequestResponse.Params.VideoLibrary;
+using KodiRPC.RPC.Specifications.Properties;
+using KodiRPC.Tests.Unit.Common;
+using KodiRPC.Tests.Unit.Common.ExpectedResults;
+using NUnit.Framework;
+
+namespace KodiRPC.Tests.Unit
+{
+    public partial class GetSeasonsTests : BaseTest
+    {
+        [Test]
+        public void GivenAJsonString_WhenGettingAllSeasons_ItShouldReturnAListOfSeasons()
+        {
+            var parameters = new GetSeasonsParams
+            {
+                TvShowId = 1,
+                Properties = new [] { SeasonProperties.ShowTitle }
+            };
+
+            var mock = GetKodiServiceMock(parameters);
+            var service = mock.Object;
+            var actual = service.GetSeasons(parameters, "UnitTests");
+            var expected = Seasons.GetList();
+
+            Assert.IsInstanceOf<GetSeasonsResponse>(actual);
+            Assert.That(actual.Result.Count, Is.EqualTo(expected.Result.Count));
+            AssertThatPropertyValuesAreEquals(actual, expected);
+        }
+    }
+}
