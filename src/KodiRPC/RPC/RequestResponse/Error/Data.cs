dd19e7f38c9e01430aa1f4656569857d6ddb61be
Pieter-Uys Fourie <pieteruys.fourie@gmail.com>
2016-07-20 16:58:59 +0200

    Enhanced errors

---
+++ src/KodiRPC/RPC/RequestResponse/Error/Data.cs
@@ -0,0 +1,14 @@
+﻿using KodiRPC.RPC.Specifications;
+using Newtonsoft.Json;
+
+namespace KodiRPC.RPC.RequestResponse.Error
+{
+    public class Data
+    {
+        [JsonProperty(PropertyName = "method")]
+        public string Method { get; set; }
+
+        [JsonProperty(PropertyName = "stack")]
+        public Stack Stack { get; set; }
+    }
+}
---
+++ src/KodiRPC/RPC/RequestResponse/Error/Stack.cs
@@ -0,0 +1,17 @@
+﻿using KodiRPC.RPC.Specifications;
+using Newtonsoft.Json;
+
+namespace KodiRPC.RPC.RequestResponse.Error
+{
+    public class Stack
+    {
+        [JsonProperty(PropertyName = "message")]
+        public string Message { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "type")]
+        public string Type { get; set; }
+    }
+}
